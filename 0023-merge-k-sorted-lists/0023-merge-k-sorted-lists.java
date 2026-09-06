32213eda97ae79fe9659910c5927364fd6863b18
Sathwik <2400030206@kluniversity.in>
2026-09-06 23:02:14 +0530

    Time: 160 ms (5.02%), Space: 46.7 MB (79.46%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,20 +10,40 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ArrayList<Integer>nums=new ArrayList<>();
-        for(ListNode temp:lists){
-            while(temp!=null){
-            nums.add(temp.val);
-            temp=temp.next;
+        if(lists==null||lists.length==0){
+            return null;
         }
+        ListNode result=lists[0];
+        for(int i=1;i<lists.length;i++){
+          result=merge(result,lists[i]);
+        }
+        return result;
     }
-      Collections.sort(nums);
-      ListNode dummy=new ListNode(0);
-    ListNode  cur=dummy;
-      for(int num:nums){
-        cur.next=new ListNode(num);
-        cur=cur.next;
-      }
+    public static ListNode merge(ListNode l1,ListNode l2){
+     ListNode dummy=new ListNode(0);
+     ListNode curr=dummy;
+
+        while(l1!=null&&l2!=null){
+            if(l1.val<=l2.val){
+             curr.next=l1;
+             l1=l1.next;
+            }
+            else{
+                curr.next=l2;
+                l2=l2.next;
+            }
+            curr=curr.next;
+        }
+        while(l1!=null){
+            curr.next=l1;
+            l1=l1.next;
+            curr=curr.next;
+        }
+        while(l2!=null){
+            curr.next=l2;
+            l2=l2.next;
+            curr=curr.next;
+        }
         return dummy.next;
     }
 }
